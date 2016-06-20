31df42c00f753b1221e2ab3ed7d75fb79b471f2c
Nick Reffitt <nickreffitt@gmail.com>
2016-06-20 11:42:04 +0100

    Update cordova interface to support delegate methods

--- www/tapdaq.js
+++ www/tapdaq.js
@@ -1,26 +1,104 @@
 
 var exec = require('cordova/exec');
 
-var Tapdaq = {};
+var tapdaqNative = {
+  service: "CDVTapdaq",
+  setOptions: "setOptions",
+  init: "init",
+  resultEvent: "event",
+  resultMessage: "message"
+};
 
-Tapdaq.setOptions = function(options) {
+var eventCallbacks = {
+  willDisplayInterstitial: "willDisplayInterstitial",
+  didDisplayInterstitial: "didDisplayInterstitial",
+  didFailToDisplayInterstitial: "didFailToDisplayInterstitial",
+  didCloseInterstitial: "didCloseInterstitial",
+  didClickInterstitial: "didClickInterstitial",
+  didFailToFetchInterstitialsFromServer: "didFailToFetchInterstitialsFromServer",
+  hasNoInterstitialsAvailable: "hasNoInterstitialsAvailable",
+  hasInterstitialsAvailableForOrientation: "hasInterstitialsAvailableForOrientation"
+};
 
-  exec(null, null, "CDVTapdaq", "setOptions", [options]);
+var that = null;
+
+var handleDelegates = function(result) {
+  var event = result[tapdaqNative.resultEvent];
+
+  if (event === eventCallbacks.willDisplayInterstitial &&
+      that.willDisplayInterstitial) {
+        that.willDisplayInterstitial();
+  }
+
+  if (event === eventCallbacks.didDisplayInterstitial &&
+      that.didDisplayInterstitial) {
+        that.didDisplayInterstitial();
+  }
+
+  if (event === eventCallbacks.didFailToDisplayInterstitial &&
+      that.didFailToDisplayInterstitial) {
+        that.didFailToDisplayInterstitial();
+  }
+
+  if (event === eventCallbacks.didCloseInterstitial &&
+      that.didCloseInterstitial) {
+        that.didCloseInterstitial();
+  }
+
+  if (event === eventCallbacks.didCloseInterstitial &&
+      that.didCloseInterstitial) {
+        that.didCloseInterstitial();
+  }
+
+  if (event === eventCallbacks.didClickInterstitial &&
+      that.didClickInterstitial) {
+        that.didClickInterstitial();
+  }
+
+  if (event === eventCallbacks.didFailToFetchInterstitialsFromServer &&
+      that.didFailToFetchInterstitialsFromServer) {
+        that.didFailToFetchInterstitialsFromServer();
+  }
+
+  if (event === eventCallbacks.hasNoInterstitialsAvailable &&
+      that.hasNoInterstitialsAvailable) {
+        that.hasNoInterstitialsAvailable();
+  }
+
+  if (event === eventCallbacks.hasInterstitialsAvailableForOrientation &&
+      that.hasInterstitialsAvailableForOrientation) {
+        var orientation = result[tapdaqNative.resultMessage]['orientation'];
+        that.hasInterstitialsAvailableForOrientation(orientation);
+  }
 
 };
 
-Tapdaq.init = function(appId, clientKey) {
+var Tapdaq = {
 
-  exec(null, null, "CDVTapdaq", "init", [
-    appId,
-    clientKey
-  ]);
+  setOptions: function(options) {
+    exec(null, null, tapdaqNative.service, tapdaqNative.setOptions, [options]);
+  },
 
-};
+  init: function(appId, clientKey) {
+    that = this;
+    exec(handleDelegates, null, tapdaqNative.service, tapdaqNative.init, [
+      appId,
+      clientKey
+    ]);
+  },
 
-Tapdaq.showInterstitial = function() {
+  showInterstitial: function() {
+    exec(null, null, tapdaqNative.service, tapdaqNative.showInterstitial, []);
+  },
 
-  exec(null, null, "CDVTapdaq", "showInterstitial", []);
+  willDisplayInterstitial: null,
+  didDisplayInterstitial: null,
+  didFailToDisplayInterstitial: null,
+  didCloseInterstitial: null,
+  didClickInterstitial: null,
+  didFailToFetchInterstitialsFromServer: null,
+  hasNoInterstitialsAvailable: null,
+  hasInterstitialsAvailableForOrientation: null
 
 };
 
