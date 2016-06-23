60fa03caa145ab0ebf6b8dfc914919f6f9fa34f7
Nick Reffitt <nickreffitt@gmail.com>
2016-06-23 16:49:01 +0100

    Remove download_ios_sdk script

--- scripts/download_ios_sdk.js
+++
@@ -1,8 +0,0 @@
-var exec = require('child_process').exec;
-
-module.exports = function(context) {
-  var pluginDir = context.opts.plugin.dir;
-  var iosLibDir = pluginDir + '/src/ios/lib/';
-
-  exec('rm -rf ' + iosLibDir + ' && git clone https://github.com/tapdaq/tapdaq-ios-sdk ' + iosLibDir + ' && rm -rf ' + iosLibDir + '/.git');
-};
